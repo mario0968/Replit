bec2f75159fcc821a4e04931825034bdff2224fe
mario0968 <louismario818@gmail.com>
2025-10-12 17:04:34 -0400

    Delete Footer.tsx

--- Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="py-4 text-center text-white backdrop-blur-md">
-      <p className="font-bold">@ {new Date().getFullYear()} REDBNB, All Rights Reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
